ff9b19a2cd2e67e556269774f887d9fb73d2b70d
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-03-07 09:28:20 +0100

    mi primer commit

--- src/modelo/GestorBBDD.java
+++ src/modelo/GestorBBDD.java
@@ -18,29 +18,6 @@ import vista.Visor;
 public class GestorBBDD {
 
 
-	public static void verClientes(String cadenaIntroducida){
-		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-		try {
-			String sql = "SELECT * FROM clientes";
-			Connection conexion = Conector.conectar();
-			Statement st = conexion.createStatement();
-			ResultSet rst = st.executeQuery(sql);
-			while(rst.next()) {
-				Cliente cliente = new Cliente();
-				cliente.setDni(rst.getString("dni"));
-				cliente.setApellido(rst.getString("apellidos"));
-				cliente.setDireccion(rst.getString("direccion"));
-				cliente.setLocalidad(rst.getString("localidad"));
-				cliente.setNombre(rst.getString("nombre"));
-				clientes.add(cliente);
-			}
-			
-			Visor.mostrarClientesqueContengan(clientes,cadenaIntroducida);
-			Conector.CERRAR();
-		} catch (SQLException e) {
-			System.out.println(e);
-		}
-	}
 
 	public static void modificarCliente(Scanner scan) {
 		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
